46eb780a81939011996df10367b20ca05f2d5422
java21bear <java21bear@gmail.com>
2026-08-06 13:37:51 +0900

    Apply gitignore

--- run.sh
+++ run.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-cd backend
+cd ../backend
+set -a
+if [ -f ".env.local" ]; then
+  source .env.local
+elif [ -f ".env" ]; then
+  source .env
+else
+  exit 1
+fi
+set +a
+if [ ! -f build/libs/compass-0.0.1.jar ]; then
+  ./gradlew build
+fi
 java -jar build/libs/compass-0.0.1.jar > /dev/null 2>&1 &
 BACKEND_PID=$!
 cd ../frontend
+if [ ! -d "node_modules" ]; then
+  npm install
+fi
 npm run dev > /dev/null 2>&1 &
 FRONTEND_PID=$!
 sleep 1
